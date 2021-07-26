3558c63697c2c3f388fad2da15e55a69e1251e45
Efrain Hernandez <efrainhg2050@gmail.com>
2021-07-26 17:37:50 -0500

    query for backward and forward buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const $video = document.querySelector('#video');
 const $playButton = document.querySelector('#play');
 const $pauseButton =  document.querySelector('#pause');
+const $backwardButton = document.querySelector('#backward');
+const $forwardButton = document.querySelector('#forward');
 
 function handlePlay() {
     $video.play();
@@ -12,6 +14,14 @@ function handlePause() {
     $video.pause();
     $pauseButton.hidden = true;
     $playButton.hidden = false;
+}
+
+function handleBackward() {
+    $video.currentTime -= 10;
+}
+
+function handleForward() {
+    $video.currentTime += 10
 }
 
 $playButton.addEventListener('click', handlePlay);
